e2d740a513155a5a3cea26327c4b81ccb08d8ff7
rjemiolo <rjemiolo@ROBERTJ-MOB.ad.wp-sa.pl>
2016-02-03 13:43:56 +0100

    Wersja 2

--- NAI/main.cpp
+++ NAI/main.cpp
@@ -1,5 +1,3 @@
-threshold demo - ksztalty/kontrast
-
 
 #include "stdafx.h"
 
@@ -7,91 +5,196 @@ threshold demo - ksztalty/kontrast
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
-//#include <string>
-//#include <sstream>
+#include <string>
+#include <sstream>
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
-#include <vector>
+#include "opencv2/objdetect.hpp"
+
 
 using namespace cv;
 
-/// Global variables
+String optionsWindow = "optionsWindow";
+String previewWindow = "previewWindow";
 
-int threshold_value = 0;
-int threshold_type = 3;;
-int const max_value = 255;
-int const max_type = 4;
-int const max_BINARY_value = 255;
+CascadeClassifier haar_cascade;
+Mat currentFrame;
+int selectedOption = 0;
+int thresholdValue = 127;
+int contourSelected = 4;
 
-Mat src, src_gray, dst;
-char* window_name = "Threshold Demo";
+	int max_thresh = 255;
+	RNG rng(12345);
 
-char* trackbar_type = "Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted";
-char* trackbar_value = "Value";
-
-/// Function headers
-void Threshold_Demo(int, void*);
-
-/**
-* @function main
-*/
-int main(int argc, char** argv)
-{
-	/// Load an image
-	//cv::VideoCapture cap(0);
-
-	src = imread("../data/fruits.jpg", 1);
-	cvtColor(src, src_gray, CV_BGR2GRAY);
-	/// Create a window to display results
-	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-
-	/// Create Trackbar to choose type of Threshold
-	createTrackbar(trackbar_type,
-		window_name, &threshold_type,
-		max_type, Threshold_Demo);
-
-	createTrackbar(trackbar_value,
-		window_name, &threshold_value,
-		max_value, Threshold_Demo);
-
-	/// Call the function to initialize
-	Threshold_Demo(0, 0);
-	
-	/// Wait until user finishes program
-	while (true)
-	{
-		//cap >> src;
+void optionChanged(int status, void* data);
+void thresh_callback(int, void*);
+void applyOption();
+void faceDetect();
 
 
-		/// Convert the image to Gray
-		//cvtColor(src, src_gray, CV_BGR2GRAY);
+void applyOption() {
+	switch (selectedOption) {
+	case 1:	
+		cvtColor(currentFrame, currentFrame, CV_BGR2GRAY);
+		break;
+	case 2:
+		cvtColor(currentFrame, currentFrame, CV_BGR2HSV);
 
-
-		int c;
-		c = waitKey(20);
-		if ((char)c == 27)
-		{
-			break;
-		}
+		break;
+	case 3:
+		threshold(currentFrame, currentFrame, thresholdValue, 255, 0);
+		break;
+	case 4:
+		thresh_callback(0,0);
+		break;
+	case 5:
+		faceDetect();
+		break;
 	}
 
 }
 
-
-/**
-* @function Threshold_Demo
-*/
-void Threshold_Demo(int, void*)
+void thresh_callback(int, void*)
 {
-	/* 0: Binary
-	1: Binary Inverted
-	2: Threshold Truncated
-	3: Threshold to Zero
-	4: Threshold to Zero Inverted
-	*/
+	Mat frameCanny, frameGray;
+	cvtColor(currentFrame, frameGray, CV_BGR2GRAY);
+	std::vector<std::vector<Point> > contours;
+	std::vector<Vec4i> hierarchy;
 
-	threshold(src_gray, dst, threshold_value, max_BINARY_value, threshold_type);
+	/// Detect edges using canny
+	Canny(frameGray, frameCanny, thresholdValue, thresholdValue * 2, 3);
+	/// Find contours
+	findContours(frameCanny, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	imshow(window_name, dst);
+	/// Draw contours
+	Mat drawing = Mat::zeros(frameCanny.size(), CV_8UC3);
+
+	
+//	Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+//	drawContours(drawing, contours, contourSelected, color, 2, 8, hierarchy, 0, Point());
+	for (int i = 0; i< contours.size(); i++)
+	{
+		//Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		Scalar color( 255, 255, 255);
+		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+	}
+	drawing.copyTo(currentFrame);
+	/// Show in a window
+//	namedWindow("Contours", WINDOW_AUTOSIZE);
+//	imshow("Contours", drawing);
 }
+
+
+void createPreviewWindow() {
+	namedWindow(previewWindow, CV_WINDOW_AUTOSIZE);
+}
+
+void createOptionsWindow() {
+	namedWindow(optionsWindow, CV_WINDOW_AUTOSIZE);
+
+	createTrackbar("Option", optionsWindow, &selectedOption, 7);//, optionChanged);
+
+	//switch (selectedOption) {
+	//	case 3:
+	//	case 4:
+			createTrackbar( "Threshold", optionsWindow, &thresholdValue, 255);
+			
+	//		break;
+	//}
+	
+}
+
+void faceDetect() {
+
+
+	Mat prev;
+
+//	cvtColor(currentFrame, gray, CV_BGR2GRAY);
+	// Find the faces in the frame:
+//	std::vector< Rect_<int> > faces;
+
+	int groundThreshold = 2;
+	double scaleStep = 1.1;
+	Size minimalObjectSize(80, 80);
+	Size maximalObjectSize(300, 300);
+	std::vector<Rect> found;
+	currentFrame.copyTo(prev);
+//	haar_cascade.detectMultiScale(gray, faces);
+	Mat image_grey;
+
+		cvtColor(currentFrame, image_grey, CV_BGR2GRAY);
+	
+		// Detect faces
+		haar_cascade.detectMultiScale(image_grey, found);// , scaleStep, groundThreshold, 0, minimalObjectSize, maximalObjectSize);
+ /*
+		if (found.size() > 0) {
+			for (int i = 0; i <= 2; i++) {
+				rectangle(prev, found[i].br(), found[i].tl(), Scalar(0, 0, 0), 1, 8, 0);
+
+			}
+		}
+// */
+// /*
+		for (int i = 0; i < found.size(); i++) {
+			// Process face by face:
+			Rect face_i = found[i];
+			// Crop the face from the image. So simple with OpenCV C++:
+			Mat face = image_grey(face_i);
+			// Resizing the face is necessary for Eigenfaces and Fisherfaces. You can easily
+			// verify this, by reading through the face recognition tutorial coming with OpenCV.
+			// Resizing IS NOT NEEDED for Local Binary Patterns Histograms, so preparing the
+			// input data really depends on the algorithm used.
+			//
+			// I strongly encourage you to play around with the algorithms. See which work best
+			// in your scenario, LBPH should always be a contender for robust face recognition.
+			//
+			// Since I am showing the Fisherfaces algorithm here, I also show how to resize the
+			// face you have just found:
+			Mat face_resized;
+			cv::resize(face, face_resized, Size(image_grey.cols, image_grey.rows), 1.0, 1.0, INTER_CUBIC);
+			
+			// And finally write all we've found out to the original image!
+			// First of all draw a green rectangle around the detected face:
+			rectangle(currentFrame, face_i, CV_RGB(0, 255, 0), 1);
+			
+		}
+// */
+		
+	//	imshow("wooohooo", prev);
+
+
+
+}
+
+void optionChanged(int status, void* data) {
+	
+	destroyWindow(optionsWindow);
+	createOptionsWindow();
+}
+
+int main(int argc, char *argv[]) {
+	
+	haar_cascade.load("../data/haarcascade_frontalface_alt.xml");
+	
+
+	createOptionsWindow();
+	createPreviewWindow();
+
+	VideoCapture input(0);
+
+
+	while (true) {
+		input >> currentFrame;
+		applyOption();
+		imshow(previewWindow, currentFrame);
+		int k = waitKey(33);
+		if (k >= 1048603) break;
+	}
+
+
+
+	return 0;
+}
+
+
